f34eab1717c81dbef1ed32af71a250159967bb51
David Zhao <David Zhao@LAPTOP-AIP8MAK5>
2018-05-10 23:23:25 -0700

    added a move randomly method to bot class

--- Bomberman/src/Bot.java
+++ Bomberman/src/Bot.java
@@ -1,17 +1,54 @@
 import processing.core.PImage;
 
-public class Bot extends Unit{
+public class Bot extends Unit {
 
 	public Bot(int lives, int speed, int xLoc, int yLoc, int width, int height, PImage img) {
 		super(lives, speed, xLoc, yLoc, width, height, img);
 		// TODO Auto-generated constructor stub
 	}
+
+	public void makeDecision(Boolean[][] dangerous) {
+		int safeSpotX;
+		int safeSpotY;
+		if (!dangerous[xLoc][yLoc]) {
+
+		} else if (!dangerous[xLoc][yLoc+1]){
+
+			/*
+			 * for (int i = 0; i < dangerous.length; i++) { for (int j = 0; j <
+			 * dangerous[i].length; j++) { if (!dangerous[i][j]) {
+			 * 
+			 * 
+			 * } }
+			 * 
+			 * }
+			 * 
+			 */
+			
+		}
+	}
 	
-	public void makeDecision( Boolean[][] dangerous) {
-		
-	
-		
-		
+	public void RandomMovements() {
+		while(this.getLives()>0) {
+		int i = (int)(Math.random()*4+1);
+		if(i == 1) {
+			this.moveXDirection((int)(Math.random()*100+1));
+		}else if(i == 2) {
+			this.moveXDirection(-(int)(Math.random()*100+1));
+		}else if(i == 3) {
+			this.moveYDirection(-(int)(Math.random()*100+1));
+		}else if(i == 4) {
+			this.moveYDirection((int)(Math.random()*100+1));
+		}else {
+			
+		}
+		try {
+			Thread.sleep(250);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}
 	}
 
 }
