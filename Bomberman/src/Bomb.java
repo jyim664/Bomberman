8f54f23ef5824bcf1494bdbc8cbb91e53e193f1a
David Zhao <David Zhao@LAPTOP-AIP8MAK5>
2018-05-12 21:02:02 -0700

    started a bomb class

---
+++ Bomberman/src/Bomb.java
@@ -0,0 +1,16 @@
+import processing.core.PImage;
+
+public class Bomb extends Unit {
+	
+	private int range;
+	
+	public Bomb( int xLoc, int yLoc, PImage img) {
+		super(1, 0, xLoc, yLoc, 30, 30, img);
+		range = 1;
+		
+	}
+
+	public void explode(){
+		
+	}
+}
